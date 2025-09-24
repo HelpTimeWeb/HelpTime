4716251f2760b07a68039868c0c0bf5165967589
Sancol610 <santycolthompson@gmail.com>
2025-09-23 22:07:00 -0300

    AHORA SI

--- helptime/helptime/settings.py
+++ helptime/helptime/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-3wvimyuvppfa$*(xe5r2h#dy_p-b7fvvak*xss+ox-r!tuuh52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['helptime.onrender.com']
+ALLOWED_HOSTS = ['helptime.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
