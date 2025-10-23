12ebea0e040f58e24b73809e93cc3a399551cb46
Sancol610 <santycolthompson@gmail.com>
2025-10-23 17:28:24 -0300

    cosas de admins

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Usuario, Servicio, Mensaje
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Servicio)
+admin.site.register(Mensaje)
